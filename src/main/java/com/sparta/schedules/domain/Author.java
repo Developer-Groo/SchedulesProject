685a6075845b7926bf6b1eb9098fb1a263d3031c
Nova <woohyeon3382@naver.com>
2024-12-06 19:43:43 +0900

    feat: create properties

--- src/main/java/com/sparta/schedules/domain/Author.java
+++ src/main/java/com/sparta/schedules/domain/Author.java
@@ -1,4 +1,21 @@
 package com.sparta.schedules.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class Author {
+
+    private final Long authorId;
+
+    private String name;
+
+    private String email;
+
+    private String createdAt;
+
+    private String updatedAt;
 }
--- src/main/java/com/sparta/schedules/domain/Schedule.java
+++ src/main/java/com/sparta/schedules/domain/Schedule.java
@@ -1,4 +1,21 @@
 package com.sparta.schedules.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class Schedule {
+
+    private final Long scheduleId;
+
+    private String todo;
+
+    private String password;
+
+    private String createdAt;
+
+    private String updatedAt;
 }
