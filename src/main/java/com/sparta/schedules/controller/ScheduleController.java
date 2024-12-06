64629b876deedcbb4268d5e6c9fa8c59ecd19f9a
Nova <woohyeon3382@naver.com>
2024-12-06 19:07:56 +0900

    feat: create domain class

--- src/main/java/com/sparta/schedules/controller/ScheduleController.java
+++ src/main/java/com/sparta/schedules/controller/ScheduleController.java
@@ -1,5 +1,6 @@
 package com.sparta.schedules.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
