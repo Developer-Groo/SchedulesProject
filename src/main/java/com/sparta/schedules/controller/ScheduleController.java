406f6c7ad9a36441e6164ad697e3cf07653aef8f
Nova <woohyeon3382@naver.com>
2024-12-07 23:23:17 +0900

    feat: controller 메서드 정의

--- src/main/java/com/sparta/schedules/controller/ScheduleController.java
+++ src/main/java/com/sparta/schedules/controller/ScheduleController.java
@@ -1,11 +1,44 @@
 package com.sparta.schedules.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sparta.schedules.domain.Schedule;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
 @RestController
-@RequestMapping("/schedules/")
+@RequestMapping("/schedules")
 public class ScheduleController {
 
+    @GetMapping
+    public ResponseEntity<List<Schedule>> getScheduleList(
+            @RequestParam(required = false) String authorId,
+            @RequestParam(required = false) LocalDateTime updatedAt,
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size
+            ) {
+        // 임시 return 값
+        return ResponseEntity.ok(List.of());
+    }
+
+    @PostMapping
+    public Schedule createSchedule(@RequestBody Schedule schedule) {
+        // 임시 return 값
+        return schedule;
+    }
+
+    @PutMapping("/{scheduleId}")
+    public String updateSchedule(@PathVariable Long scheduleId, @RequestBody Schedule schedule) {
+        // 임시 return 값
+        return "OK";
+    }
+
+    @DeleteMapping("/{scheduleId}")
+    public ResponseEntity<Schedule> deleteSchedule(@PathVariable Long scheduleId, @RequestBody Schedule schedule) {
+        // 임시 return 값
+        return ResponseEntity.ok(new Schedule());
+    }
 }
