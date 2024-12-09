089d0adccd389f456229d4b99a96de663aa1a648
Nova <woohyeon3382@naver.com>
2024-12-09 19:18:51 +0900

    feat: AuthorController 구현

--- src/main/java/com/sparta/schedules/controller/AuthorController.java
+++ src/main/java/com/sparta/schedules/controller/AuthorController.java
@@ -1,19 +1,26 @@
 package com.sparta.schedules.controller;
 
+import com.sparta.schedules.domain.Author;
+import com.sparta.schedules.service.AuthorService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/schedules")
 public class AuthorController {
 
+    private final AuthorService authorService;
+
     @GetMapping("/author_id={authorId}")
-    public ResponseEntity<String> findByAuthorId(@PathVariable Long authorId) {
-        // authorservice 에서 findbyid 메서드 호출
-        // 반환된 author 객체 return
-        return ResponseEntity.ok("");
+    public ResponseEntity<Author> findByAuthorId(@PathVariable Long authorId) {
+        Optional<Author> byId = authorService.findById(authorId);
+        return ResponseEntity.ok(byId.get());
     }
 }
