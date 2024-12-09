27348c31258089057a33bf55a5edcd43238d0448
Nova <woohyeon3382@naver.com>
2024-12-09 23:16:37 +0900

    refactor: schedule 이 조회되도록 변경
    - 기존 authorId 로 author 테이블의 데이터가 조회되는 로직에서 authorId 로 schedule 테이블의 데이터가 조회되도록 변경

--- src/main/java/com/sparta/schedules/repository/JdbcAuthorRepository.java
+++ src/main/java/com/sparta/schedules/repository/JdbcAuthorRepository.java
@@ -1,6 +1,7 @@
 package com.sparta.schedules.repository;
 
 import com.sparta.schedules.domain.Author;
+import com.sparta.schedules.domain.Schedule;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
@@ -20,21 +21,23 @@ public class JdbcAuthorRepository {
         this.template = new NamedParameterJdbcTemplate(dataSource);
     }
 
-    public Optional<Author> findById(Long authorId) {
-        String sql = "select author_id, name, email, created_at, updated_at " +
-                "from author where author_id = :authorId";
+    public Optional<Schedule> findById(Long authorId) {
+        String sql = "select s.schedule_id, s.todo, s.created_at, s.updated_at, a.author_id " +
+                "from schedule s " +
+                "join author a on s.author_id = a.author_id " +
+                "where a.author_id = :authorId";
 
         try {
             HashMap<String, Object> param = new HashMap<>();
             param.put("authorId", authorId);
-            Author author = template.queryForObject(sql, param, rowMapper());
-            return Optional.of(author);
+            Schedule schedule = template.queryForObject(sql, param, rowMapper());
+            return Optional.of(schedule);
         } catch (EmptyResultDataAccessException e) {
             return Optional.empty();
         }
     }
 
-    private RowMapper<Author> rowMapper() {
-        return BeanPropertyRowMapper.newInstance(Author.class);
+    private RowMapper<Schedule> rowMapper() {
+        return BeanPropertyRowMapper.newInstance(Schedule.class);
     }
 }
