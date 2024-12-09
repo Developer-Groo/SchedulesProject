58da76a50c66dc149e74a646a1ded8072f41f352
Nova <woohyeon3382@naver.com>
2024-12-09 23:40:27 +0900

    refactor: 공통 예외처리를 위한 @ResponseStatus 삭제

--- src/main/java/com/sparta/schedules/exception/IncorrectPasswordException.java
+++ src/main/java/com/sparta/schedules/exception/IncorrectPasswordException.java
@@ -1,9 +1,5 @@
 package com.sparta.schedules.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Incorrect password")
 public class IncorrectPasswordException extends RuntimeException {
     public IncorrectPasswordException(String message) {
         super(message);
