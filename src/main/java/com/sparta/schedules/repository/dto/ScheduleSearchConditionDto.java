7376fd1b4f400c1d768ce9564fe4427fea1e0477
Nova <woohyeon3382@naver.com>
2024-12-09 18:29:26 +0900

    feat: ScheduleSearchConditionDto 구현

--- src/main/java/com/sparta/schedules/repository/dto/ScheduleSearchConditionDto.java
+++ src/main/java/com/sparta/schedules/repository/dto/ScheduleSearchConditionDto.java
@@ -1,4 +1,19 @@
 package com.sparta.schedules.repository.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class ScheduleSearchConditionDto {
+
+    private String name;
+    private LocalDateTime updatedAt;
+
+    public ScheduleSearchConditionDto(String name, LocalDateTime updatedAt) {
+        this.name = name;
+        this.updatedAt = updatedAt;
+    }
 }
