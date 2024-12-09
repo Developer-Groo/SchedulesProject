c048f278f86eafc9838bdbeedc398315898c24e0
Nova <woohyeon3382@naver.com>
2024-12-09 18:29:06 +0900

    refactor: @RequiredArgsConstructor 제거

--- src/main/java/com/sparta/schedules/repository/dto/ScheduleUpdateDto.java
+++ src/main/java/com/sparta/schedules/repository/dto/ScheduleUpdateDto.java
@@ -1,12 +1,10 @@
 package com.sparta.schedules.repository.dto;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@RequiredArgsConstructor
 public class ScheduleUpdateDto {
 
     private String todo;
