8a58f853745c455efca6f93b0f3383be489d9430
Nova <woohyeon3382@naver.com>
2024-12-09 19:48:52 +0900

    feat: paging 을 위한 프로퍼티 추가

--- src/main/java/com/sparta/schedules/repository/dto/ScheduleSearchConditionDto.java
+++ src/main/java/com/sparta/schedules/repository/dto/ScheduleSearchConditionDto.java
@@ -1,19 +1,22 @@
 package com.sparta.schedules.repository.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ScheduleSearchConditionDto {
 
     private String name;
     private LocalDateTime updatedAt;
 
-    public ScheduleSearchConditionDto(String name, LocalDateTime updatedAt) {
-        this.name = name;
-        this.updatedAt = updatedAt;
-    }
+    @Builder.Default
+    private int page = 1;
+
+    @Builder.Default
+    private int size = 10;
 }
