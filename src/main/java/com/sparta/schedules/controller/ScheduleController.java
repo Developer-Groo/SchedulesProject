bde5ede487fc4805f08f299795b814e50de19f1d
Nova <woohyeon3382@naver.com>
2024-12-09 18:30:09 +0900

    refactor: api 경로 변경 및 메서드 구현

--- src/main/java/com/sparta/schedules/controller/ScheduleController.java
+++ src/main/java/com/sparta/schedules/controller/ScheduleController.java
@@ -2,16 +2,16 @@ package com.sparta.schedules.controller;
 
 import com.sparta.schedules.domain.Schedule;
 import com.sparta.schedules.repository.dto.ScheduleRequestDto;
+import com.sparta.schedules.repository.dto.ScheduleSearchConditionDto;
+import com.sparta.schedules.repository.dto.ScheduleUpdateDto;
 import com.sparta.schedules.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/schedules")
@@ -20,32 +20,32 @@ public class ScheduleController {
     private final ScheduleService scheduleService;
 
     @GetMapping
-    public ResponseEntity<List<Schedule>> getScheduleList(
-            @RequestParam(required = false) String authorId,
-            @RequestParam(required = false) LocalDateTime updatedAt,
-            @RequestParam(defaultValue = "1") int page,
-            @RequestParam(defaultValue = "10") int size
-            ) {
-        // 임시 return 값
-        return ResponseEntity.ok(List.of());
+    public ResponseEntity<List<Schedule>> getScheduleList(ScheduleSearchConditionDto conditionDto) {
+        List<Schedule> scheduleList = scheduleService.findAll(conditionDto);
+        return ResponseEntity.ok(scheduleList);
+    }
+
+    @GetMapping("/schedule_id={scheduleId}")
+    public ResponseEntity<Schedule> findByScheduleId(@PathVariable Long scheduleId) {
+        Optional<Schedule> byId = scheduleService.findById(scheduleId);
+        return ResponseEntity.ok(byId.get());
     }
 
     @PostMapping
     public ResponseEntity<Schedule> createSchedule(@RequestBody ScheduleRequestDto requestDto) {
         Schedule savedSchedule = scheduleService.save(requestDto);
-
         return ResponseEntity.ok(savedSchedule);
     }
 
-    @PutMapping("/{scheduleId}")
-    public String updateSchedule(@PathVariable Long scheduleId, @RequestBody Schedule schedule) {
-        // 임시 return 값
+    @PutMapping("/schedule_id={scheduleId}")
+    public String updateSchedule(@PathVariable Long scheduleId, @RequestBody ScheduleUpdateDto updateDto) {
+        scheduleService.update(scheduleId, updateDto);
         return "OK";
     }
 
-    @DeleteMapping("/{scheduleId}")
-    public ResponseEntity<Schedule> deleteSchedule(@PathVariable Long scheduleId) {
-        // 임시 return 값
-        return ResponseEntity.ok(new Schedule());
+    @DeleteMapping("/schedule_id={scheduleId}")
+    public ResponseEntity<Boolean> deleteSchedule(@PathVariable Long scheduleId, @RequestBody String password) {
+        boolean delete = scheduleService.delete(scheduleId, password);
+        return ResponseEntity.ok(delete);
     }
 }
