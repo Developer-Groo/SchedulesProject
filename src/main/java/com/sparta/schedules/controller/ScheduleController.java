e9a8990a3170f0479d943c2da9658529ba01d889
Nova <woohyeon3382@naver.com>
2024-12-09 21:27:41 +0900

    docs: validation 의존성 추가

--- src/main/java/com/sparta/schedules/controller/ScheduleController.java
+++ src/main/java/com/sparta/schedules/controller/ScheduleController.java
@@ -5,6 +5,7 @@ import com.sparta.schedules.repository.dto.ScheduleRequestDto;
 import com.sparta.schedules.repository.dto.ScheduleSearchConditionDto;
 import com.sparta.schedules.repository.dto.ScheduleUpdateDto;
 import com.sparta.schedules.service.ScheduleService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -32,13 +33,13 @@ public class ScheduleController {
     }
 
     @PostMapping
-    public ResponseEntity<Schedule> createSchedule(@RequestBody ScheduleRequestDto requestDto) {
+    public ResponseEntity<Schedule> createSchedule(@RequestBody @Valid ScheduleRequestDto requestDto) {
         Schedule savedSchedule = scheduleService.save(requestDto);
         return ResponseEntity.ok(savedSchedule);
     }
 
     @PutMapping("/schedule_id={scheduleId}")
-    public String updateSchedule(@PathVariable Long scheduleId, @RequestBody ScheduleUpdateDto updateDto) {
+    public String updateSchedule(@PathVariable Long scheduleId, @RequestBody @Valid ScheduleUpdateDto updateDto) {
         scheduleService.update(scheduleId, updateDto);
         return "OK";
     }
