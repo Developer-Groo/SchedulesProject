952e2c7ecb88e13045b16676e84b572d8c0d206d
Groo <woohyeon3382@naver.com>
2024-12-19 05:49:54 +0900

    feat: filter 등록을 위한 FilterConfig 구현

---
+++ src/main/java/com/sparta/schedules/web/config/FilterConfig.java
@@ -0,0 +1,21 @@
+package com.sparta.schedules.web.config;
+
+import com.sparta.schedules.web.LoginFilter;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean loginFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+}
