dc99444570dc3c96fdd5b791d519e1d23999aca1
Groo <woohyeon3382@naver.com>
2024-12-19 05:53:54 +0900

    refactor: password 필드 제거

--- src/main/java/com/sparta/schedules/dto/schedule/request/ScheduleUpdateDto.java
+++ src/main/java/com/sparta/schedules/dto/schedule/request/ScheduleUpdateDto.java
@@ -16,7 +16,4 @@ public class ScheduleUpdateDto {
     @NotBlank(message = "할일(todo)은 필수입니다.")
     @Size(max = 200, message = "할일(todo)은 최대 200자까지 입력 가능합니다.")
     private String todoContent;
-
-    @NotBlank(message = "비밀번호는 필수입니다.")
-    private String password;
 }
