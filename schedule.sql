057368fd7c68ab41a4fb2f49ae7c81407318f051
Groo <woohyeon3382@naver.com>
2024-12-18 16:31:52 +0900

    docs: sql 문서 수정

--- schedule.sql
+++ schedule.sql
@@ -1,19 +1,33 @@
-# 작성자 생성
-CREATE TABLE author (
-                        author_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                        name VARCHAR(50) NOT NULL,
-                        email VARCHAR(50) NOT NULL UNIQUE,
-                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+# 유저 생성
+CREATE TABLE user
+(
+    user_id    BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name       VARCHAR(50)  NOT NULL,
+    email      VARCHAR(50)  NOT NULL UNIQUE,
+    password   VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NULL
 );
 
 # 일정 생성
-CREATE TABLE schedule (
-                          schedule_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                          todo VARCHAR(200) NOT NULL,
-                          password VARCHAR(255) NOT NULL,
-                          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                          updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                          author_id BIGINT NOT NULL,
-                          FOREIGN KEY (author_id) REFERENCES author(author_id) ON DELETE CASCADE
+CREATE TABLE schedule
+(
+    schedule_id  BIGINT AUTO_INCREMENT PRIMARY KEY,
+    todo_title   VARCHAR(100) NOT NULL,
+    todo_content TEXT         NOT NULL,
+    created_at   TIMESTAMP NULL,
+    updated_at   TIMESTAMP NULL,
+    user_id      BIGINT       NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE
+);
+
+# 댓글 생성
+CREATE TABLE comment
+(
+    comment_id      BIGINT AUTO_INCREMENT PRIMARY KEY,
+    comment_content TEXT      NOT NULL,
+    created_at      TIMESTAMP NULL,
+    updated_at      TIMESTAMP NULL,
+    user_id         BIGINT    NOT NULL,
+    schedule_id     BIGINT    NOT NULL,
+    FOREIGN KEY (schedule_id) REFERENCES schedule (schedule_id) ON DELETE CASCADE
 );
