1404bf79b801527089eae06809d347d126d2f61a
Groo <woohyeon3382@naver.com>
2024-12-18 23:18:41 +0900

    refactor: UserService 수정

--- src/main/java/com/sparta/schedules/service/UserService.java
+++ src/main/java/com/sparta/schedules/service/UserService.java
@@ -1,19 +1,55 @@
 package com.sparta.schedules.service;
 
-import com.sparta.schedules.domain.Schedule;
+import com.sparta.schedules.domain.User;
+import com.sparta.schedules.dto.user.request.UserSearchConditionDto;
+import com.sparta.schedules.dto.user.request.UserUpdateDto;
+import com.sparta.schedules.dto.user.response.UserResponseDto;
 import com.sparta.schedules.exception.NotFoundException;
-import com.sparta.schedules.repository.jdbc.JdbcUserRepository;
+import com.sparta.schedules.repository.user.UserDynamicQueryRepository;
+import com.sparta.schedules.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-    private final JdbcUserRepository repository;
+    private final UserRepository repository;
+    private final UserDynamicQueryRepository queryRepository;
 
-    public Schedule findById(Long userId) {
+    // 유저 생성
+    public User save(User user) {
+        return repository.save(user);
+    }
+
+    // 유저 조회 (ID)
+    public User findById(Long userId) {
         return repository.findById(userId)
                 .orElseThrow(() -> new NotFoundException("Requested ID not found"));
+    }
+
+    // 유저 조회 (Name)
+    public List<UserResponseDto> findByName(UserSearchConditionDto conditionDto) {
+        Pageable pageRequest = PageRequest.of(conditionDto.getPage(), conditionDto.getSize());
+        return queryRepository.findByName(conditionDto, pageRequest);
+    }
+
+    // 유저 수정
+    public void update(Long userId, UserUpdateDto updateDto) {
+        User findUser = findById(userId);
+
+        findUser.setName(updateDto.getName());
+        findUser.setEmail(updateDto.getEmail());
+        findUser.setPassword(updateDto.getPassword());
+    }
+
+    // 유저 삭제
+    public void delete(Long user) {
+        User findUser = findById(user);
+        repository.delete(findUser);
     }
 }
