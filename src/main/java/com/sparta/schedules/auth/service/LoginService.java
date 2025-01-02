3413d0711fe9815e3b47df721e779c0d59a2e4a6
Groo <woohyeon3382@naver.com>
2025-01-02 19:36:46 +0900

    feat: Transctional 적용

--- src/main/java/com/sparta/schedules/auth/service/LoginService.java
+++ src/main/java/com/sparta/schedules/auth/service/LoginService.java
@@ -1,12 +1,17 @@
-package com.sparta.schedules.web.login;
+package com.sparta.schedules.auth.service;
 
+import com.sparta.schedules.auth.dto.request.SignUpDto;
+import com.sparta.schedules.auth.dto.response.SuccessResponseDto;
 import com.sparta.schedules.user.entity.User;
 import com.sparta.schedules.user.dto.response.UserResponseDto;
 import com.sparta.schedules.exception.IncorrectPasswordException;
 import com.sparta.schedules.user.repository.UserRepository;
-import com.sparta.schedules.web.config.PasswordEncoder;
+import com.sparta.schedules.auth.util.PasswordEncoder;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +20,16 @@ public class LoginService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Transactional
+    public SuccessResponseDto signUp(SignUpDto dto) {
+        User user = new User(dto.name(), dto.email(), dto.password());
+
+        User savedUser = userRepository.save(user);
+
+        return new SuccessResponseDto(HttpServletResponse.SC_CREATED, "회원가입 성공");
+    }
+
+    @Transactional
     public UserResponseDto login(String email, String password) {
         User user = userRepository.findAll().stream()
                 .filter(u -> u.getEmail().equals(email))
