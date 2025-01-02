be61b3bc12241ff769ac0b3d55391bef5eed5b73
Groo <woohyeon3382@naver.com>
2025-01-02 19:33:36 +0900

    feat: signUp 메서드 구현

--- src/main/java/com/sparta/schedules/auth/controller/LoginController.java
+++ src/main/java/com/sparta/schedules/auth/controller/LoginController.java
@@ -1,12 +1,17 @@
-package com.sparta.schedules.web.login;
+package com.sparta.schedules.auth.controller;
 
-import com.sparta.schedules.auth.dto.SuccessResponseDto;
+import com.sparta.schedules.auth.dto.response.SuccessResponseDto;
+import com.sparta.schedules.auth.dto.request.SignUpDto;
 import com.sparta.schedules.user.dto.response.UserResponseDto;
-import com.sparta.schedules.web.config.SessionAttribute;
+import com.sparta.schedules.auth.web.config.SessionAttribute;
+import com.sparta.schedules.auth.dto.request.LoginForm;
+import com.sparta.schedules.auth.service.LoginService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,12 +19,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/home")
 public class LoginController {
 
     private final LoginService loginService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<SuccessResponseDto> signUp(@RequestBody @Valid SignUpDto dto) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(loginService.signUp(dto));
+    }
 
     @PostMapping("/login")
     public ResponseEntity<Object> login(@RequestBody @Valid LoginForm form, HttpServletRequest request) {
@@ -32,7 +45,8 @@ public class LoginController {
         HttpSession session = request.getSession();
         session.setAttribute(SessionAttribute.LOGIN_MEMBER.getKey(), user);
 
-        return ResponseEntity.ok(new SuccessResponseDto("로그인 성공", "/home/schedules"));
+        return ResponseEntity
+                .ok(new SuccessResponseDto(HttpServletResponse.SC_OK, "로그인 성공"));
     }
 
     @PostMapping("/logout")
@@ -41,6 +55,8 @@ public class LoginController {
         if (session != null) {
             session.invalidate();
         }
-        return ResponseEntity.ok(new SuccessResponseDto("로그아웃 성공", null));
+
+        return ResponseEntity
+                .ok(new SuccessResponseDto(HttpServletResponse.SC_OK, "로그아웃 성공"));
     }
 }
